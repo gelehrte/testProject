e63b87e22f1f5820f0127cde7ddfb628bf722bf5
gelehrte <gele@an.wakwak.com>
2017-02-11 20:25:26 +0900

    第一回Python説明準備

--- gele/views.py
+++ gele/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def test_one(request):
+    """書籍の一覧"""
+    #    return HttpResponse('書籍の一覧')
+    return render(request,
+                  'test_one/test_one.html',  # 使用するテンプレート
+                  )  # テンプレートに渡すデータ
